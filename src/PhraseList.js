b61e234f91c56daba6d2512a576592c863ff2b5b
Alan Hogan <contact@alanhogan.com>
2017-08-29 17:38:08 -0700

    Sort based on first valid phrase, not spaceless key

--- src/PhraseList.js
+++ src/PhraseList.js
@@ -9,7 +9,11 @@ class PhraseList extends Component {
   render() {
     return (
       <div>
-        {Object.keys(phrases).sort().map(function(key){
+        {Object.keys(phrases).sort((a, b) => {
+          return phrases[a].valid[0].phrase.localeCompare(
+            phrases[b].valid[0].phrase
+          )
+        }).map(function(key){
           return <Phrase phrase={phrases[key]} />;
         })}
       </div>
